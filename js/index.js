ffec1e0f8f676c51782c89a3be92e596ad3d2e1a
kevin Gasta <kevin.gastaldello21@gmail.com>
2023-01-25 21:17:58 -0300

    perfeccionamos el formulario

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
 const moverASeccion = (seccion) => {
     document.getElementById(seccion).scrollIntoView();
 }
+
+const validarFormulario = () => {
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let mail = document.getElementById("mail").value;
+    let telefono = document.getElementById("telefono").value;
+    let mensaje = document.getElementById("mensaje").value;
+  
+    if (nombre != "" && apellido != "" && mail != "" && telefono != "" && mensaje != "") {
+      alert("Mensaje enviado");
+    }
+  }
